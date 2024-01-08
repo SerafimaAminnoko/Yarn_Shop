8030401d9446527efd7164e374760245fd10b7f8
SerafimaAminnoko <132136596+SerafimaAminnoko@users.noreply.github.com>
2024-01-08 13:50:12 +0200

    added first url_test.

--- yarn_shop/shop/tests/test_urls.py
+++ yarn_shop/shop/tests/test_urls.py
@@ -1,6 +1,11 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from shop.views import ProductList
 
 
 class UrlTestCase(SimpleTestCase):
     def main_page(self):
-        assert 1 == 2
+        url = reverse('')
+        print(url)
+        self.assertEquals(resolve(url).func.view_class, ProductList)
